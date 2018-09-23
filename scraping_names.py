45c290ad89fe086718616a21c08ebf38151b5905
Koldo <koldopinaortiz@gmail.com>
2018-09-23 19:58:31 +0200

    Starting new proyect

---
+++ scraping_names.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Koldo Pina
+Date created: 23/09/2018
+Python Version: 3.6
+"""
+
+url = 'http://www.dimfuture.net/starwars/random/generate.php'
