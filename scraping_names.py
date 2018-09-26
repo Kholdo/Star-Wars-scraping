83843865697818f71397b66f925a44f3df5c8a6a
Koldo <koldopinaortiz@gmail.com>
2018-09-26 09:09:17 +0200

    using Selenium to click radio button

--- scraping_names.py
+++ scraping_names.py
@@ -6,4 +6,20 @@ Date created: 23/09/2018
 Python Version: 3.6
 """
 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
 url = 'http://www.dimfuture.net/starwars/random/generate.php'
+driver.get(url)
+
+cien =driver.find_element_by_xpath("//input[@name='choice' and @value='100']")
+
+
+cien.click()
+print ('click')
+
+# time.sleep(6)
+
+driver.close()
