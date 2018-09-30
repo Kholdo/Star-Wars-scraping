dc84f63519647aaf1396bd79a1c1c7e8dd993c5c
Koldo <koldopinaortiz@gmail.com>
2018-09-30 19:22:22 +0200

    get html

--- scraping_planets.py
+++ scraping_planets.py
@@ -7,8 +7,13 @@ Python Version: 3.6
 """
 
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 url_planets = 'http://starwars.wikia.com/wiki/List_of_planets'
+url_planets_req = requests.get(url_planets)
+planets_html = url_planets_req.text
+planets_soup = BeautifulSoup(planets_html, "html.parser")
+
 
